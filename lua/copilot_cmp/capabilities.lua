4ba220da7df2ab0469908e42a32b68febb9265fe
Lateef Alabi-Oki <mystilleef@gmail.com>
2023-12-05 12:07:38 -0500

    style: format

--- lua/copilot_cmp/capabilities.lua
+++ lua/copilot_cmp/capabilities.lua
@@ -4,22 +4,25 @@
 local M = {}
 
 local if_nil = function(val, default)
-  if val == nil then return default end
+  if val == nil then
+    return default
+  end
   return val
 end
 
 -- Backported from vim.deprecate (0.9.0+)
 local function deprecate(name, alternative, version, plugin, backtrace)
-  local message = name .. ' is deprecated'
-  plugin = plugin or 'Nvim'
-  message = alternative and (message .. ', use ' .. alternative .. ' instead.') or message
+  local message = name .. " is deprecated"
+  plugin = plugin or "Nvim"
+  message = alternative and (message .. ", use " .. alternative .. " instead.")
+    or message
   message = message
-    .. ' See :h deprecated\nThis function will be removed in '
+    .. " See :h deprecated\nThis function will be removed in "
     .. plugin
-    .. ' version '
+    .. " version "
     .. version
   if vim.notify_once(message, vim.log.levels.WARN) and backtrace ~= false then
-    vim.notify(debug.traceback('', 2):sub(2), vim.log.levels.WARN)
+    vim.notify(debug.traceback("", 2):sub(2), vim.log.levels.WARN)
   end
 end
 
@@ -32,27 +35,30 @@ M.default_capabilities = function(override)
         dynamicRegistration = if_nil(override.dynamicRegistration, false),
         completionItem = {
           snippetSupport = if_nil(override.snippetSupport, true),
-          commitCharactersSupport = if_nil(override.commitCharactersSupport, true),
+          commitCharactersSupport = if_nil(
+            override.commitCharactersSupport,
+            true
+          ),
           deprecatedSupport = if_nil(override.deprecatedSupport, true),
           preselectSupport = if_nil(override.preselectSupport, true),
           tagSupport = if_nil(override.tagSupport, {
             valueSet = {
               1, -- Deprecated
-            }
+            },
           }),
           insertReplaceSupport = if_nil(override.insertReplaceSupport, true),
           resolveSupport = if_nil(override.resolveSupport, {
-              properties = {
-                  "documentation",
-                  "detail",
-                  "additionalTextEdits",
-              },
+            properties = {
+              "documentation",
+              "detail",
+              "additionalTextEdits",
+            },
           }),
           insertTextModeSupport = if_nil(override.insertTextModeSupport, {
             valueSet = {
               1, -- asIs
               2, -- adjustIndentation
-            }
+            },
           }),
           labelDetailsSupport = if_nil(override.labelDetailsSupport, true),
         },
@@ -60,13 +66,13 @@ M.default_capabilities = function(override)
         insertTextMode = if_nil(override.insertTextMode, 1),
         completionList = if_nil(override.completionList, {
           itemDefaults = {
-            'commitCharacters',
-            'editRange',
-            'insertTextFormat',
-            'insertTextMode',
-            'data',
-          }
-        })
+            "commitCharacters",
+            "editRange",
+            "insertTextFormat",
+            "insertTextMode",
+            "data",
+          },
+        }),
       },
     },
   }
@@ -75,7 +81,12 @@ end
 ---Backwards compatibility
 M.update_capabilities = function(_, override)
   local _deprecate = vim.deprecate or deprecate
-  _deprecate('copilot_cmp.update_capabilities', 'copilot_cmp.default_capabilities', '1.0.0', 'copilot_cmp')
+  _deprecate(
+    "copilot_cmp.update_capabilities",
+    "copilot_cmp.default_capabilities",
+    "1.0.0",
+    "copilot_cmp"
+  )
   return M.default_capabilities(override)
 end
 
--- lua/copilot_cmp/source.lua
+++ lua/copilot_cmp/source.lua
@@ -2,16 +2,16 @@ local source = {
   executions = {},
 }
 
-function source:get_keyword_pattern()
-  return '.'
+function source.get_keyword_pattern()
+  return "."
 end
 
 source.get_trigger_characters = function()
-  return {'.'}
+  return { "." }
 end
 
 -- executes before selection
-source.resolve = function (self, completion_item, callback)
+source.resolve = function(self, completion_item, callback)
   for _, fn in ipairs(self.executions) do
     completion_item = fn(completion_item)
   end
@@ -24,34 +24,32 @@ source.is_available = function(self)
     return false
   end
   -- client is not attached to current buffer.
-  local active_clients = vim.lsp.get_active_clients({ bufnr = vim.api.nvim_get_current_buf() })
+  local active_clients =
+    vim.lsp.get_active_clients({ bufnr = vim.api.nvim_get_current_buf() })
   local active_copilot_client = vim.tbl_filter(function(client)
     return client.id == self.client.id
   end, active_clients)
   if next(active_copilot_client) == nil then
     return false
   end
-  if not self.client.name == "copilot" then
+  if self.client.name ~= "copilot" then
     return false
   end
   return true
 end
 
-source.execute = function (self, completion_item, callback)
+source.execute = function(_, completion_item, callback)
   callback(completion_item)
 end
 
 source.new = function(client, opts)
   local completion_functions = require("copilot_cmp.completion_functions")
-
   local self = setmetatable({
-    timer = vim.loop.new_timer()
+    timer = vim.loop.new_timer(),
   }, { __index = source })
-
   self.client = client
   self.request_ids = {}
-  self.complete = completion_functions.init('getCompletionsCycling', opts)
-
+  self.complete = completion_functions.init("getCompletionsCycling", opts)
   return self
 end
 
